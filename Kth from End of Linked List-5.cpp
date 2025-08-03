dc22fad4893d4ae07fdba4001cd06d6185e8ce3e
sagar patidar <152028847+Sagarpatidar407@users.noreply.github.com>
2025-08-03 23:00:11 +0530

    Linked List Problems - Kth Node from the End of a Linked List
    🧾 Problem: Kth Node from the End of a Linked List - https://www.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1 
    You are given the head of a singly linked list and an integer k.
    Your task is to find and return the data of the k-th node from the end of the list.
    
    If k is greater than the total number of nodes, return -1.
    
    🔍 Examples
    Input:
    Linked List: 1 → 2 → 3 → 4 → 5 → 6 → 7 → 8 → 9
    k = 2
    Output: 8
    Explanation: The 2nd node from the end is 8.
    
    Input:
    Linked List: 10 → 5 → 100 → 5
    k = 5
    Output: -1
    Explanation: The list has only 4 nodes. Since k = 5 is more than the length of the list, return -1.
    
    ✅ Constraints
    1 ≤ Number of nodes ≤ 10^5
    
    1 ≤ k ≤ 10^5
    
    Node data values are integers.
    
    💡 Your Task
    Implement the following function:
    
    cpp
    Copy
    Edit
    int getKthFromLast(Node* head, int k);
    head: pointer to the first node of the linked list
    
    k: the position from the end (1-based index)
    
    Return the data of the k-th node from the end. If not possible, return -1.

---
+++ Kth from End of Linked List-5.cpp
@@ -0,0 +1,30 @@
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+class Solution {
+  public:
+    int getKthFromLast(Node* head, int k) {
+        
+        Node* fast = head;
+        Node* slow = head;
+        
+        for(int i=0;i<k;i++){
+            if(fast==NULL) return -1;
+            fast = fast->next;
+        }
+        
+        while(fast!=NULL){
+            slow = slow->next;
+            fast = fast->next;  
+        }
+        
+        return (slow!=NULL) ? slow->data : -1;
+        
+    }
+};
